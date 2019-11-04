013fbc2c31a0b2926acbb322b6b47d191d4303ae
larrynow <llfwyyx@163.com>
2019-11-04 13:00:46 +0800

    Use indices buffer reduce vertices buffer size in CreateCube.

--- Source/BFGeometry.cpp
+++ Source/BFGeometry.cpp
@@ -14,52 +14,65 @@ void BFGeometry::CreateCube(const float width, std::vector<Vertex>& vertices, st
 	vertices.push_back({ VEC3(-width/2, -width/2, -width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, 0.f, -1.f), VEC2(0.f, 0.f) });// Bottom-left.
 	vertices.push_back({ VEC3( width/2,  width/2, -width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, 0.f, -1.f), VEC2(1.f, 1.f) });// Top-right.
 	vertices.push_back({ VEC3( width/2, -width/2, -width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, 0.f, -1.f), VEC2(1.f, 0.f) });// Bottom-right.
-	vertices.push_back({ VEC3( width/2,  width/2, -width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, 0.f, -1.f), VEC2(1.f, 1.f) });// Top-right.
-	vertices.push_back({ VEC3(-width/2, -width/2, -width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, 0.f, -1.f), VEC2(0.f, 0.f) });// Bottom-left.
+	//vertices.push_back({ VEC3( width/2,  width/2, -width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, 0.f, -1.f), VEC2(1.f, 1.f) });// Top-right.
+	//vertices.push_back({ VEC3(-width/2, -width/2, -width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, 0.f, -1.f), VEC2(0.f, 0.f) });// Bottom-left.
 	vertices.push_back({ VEC3(-width/2,  width/2, -width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, 0.f, -1.f), VEC2(0.f, 1.f) });// Top-left.
+
+	// Clockwise: 0, 1, 2; 1, 0, 3;
 
 	// Back face.
 	vertices.push_back({ VEC3(-width/2, -width/2, width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, 0.f, 1.f), VEC2(0.f, 0.f) });
 	vertices.push_back({ VEC3( width/2, -width/2, width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, 0.f, 1.f), VEC2(1.f, 0.f) });
 	vertices.push_back({ VEC3( width/2,  width/2, width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, 0.f, 1.f), VEC2(1.f, 1.f) });
-	vertices.push_back({ VEC3( width/2,  width/2, width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, 0.f, 1.f), VEC2(1.f, 1.f) });
+	//vertices.push_back({ VEC3( width/2,  width/2, width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, 0.f, 1.f), VEC2(1.f, 1.f) });
 	vertices.push_back({ VEC3(-width/2,  width/2, width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, 0.f, 1.f), VEC2(0.f, 1.f) });
-	vertices.push_back({ VEC3(-width/2, -width/2, width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, 0.f, 1.f), VEC2(0.f, 0.f) });
+	//vertices.push_back({ VEC3(-width/2, -width/2, width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, 0.f, 1.f), VEC2(0.f, 0.f) });
+
+	// 4, 5, 6; 6, 7, 4;
 
 	// Left face.
 	vertices.push_back({ VEC3(-width/2,  width/2,  width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(-1.f, 0.f, 0.f), VEC2(1.f, 0.f) });
 	vertices.push_back({ VEC3(-width/2,  width/2, -width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(-1.f, 0.f, 0.f), VEC2(1.f, 1.f) });
 	vertices.push_back({ VEC3(-width/2, -width/2, -width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(-1.f, 0.f, 0.f), VEC2(0.f, 1.f) });
-	vertices.push_back({ VEC3(-width/2, -width/2, -width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(-1.f, 0.f, 0.f), VEC2(0.f, 1.f) });
+	//vertices.push_back({ VEC3(-width/2, -width/2, -width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(-1.f, 0.f, 0.f), VEC2(0.f, 1.f) });
 	vertices.push_back({ VEC3(-width/2, -width/2,  width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(-1.f, 0.f, 0.f), VEC2(0.f, 0.f) });
-	vertices.push_back({ VEC3(-width/2,  width/2,  width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(-1.f, 0.f, 0.f), VEC2(1.f, 0.f) });
+	//vertices.push_back({ VEC3(-width/2,  width/2,  width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(-1.f, 0.f, 0.f), VEC2(1.f, 0.f) });
+
+	// 8, 9, 10; 10, 11, 8;
 
 	// Right face.
 	vertices.push_back({ VEC3(width/2,  width/2,  width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(1.f, 0.f, 0.f), VEC2(1.f, 0.f) });
 	vertices.push_back({ VEC3(width/2, -width/2, -width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(1.f, 0.f, 0.f), VEC2(0.f, 1.f) });
 	vertices.push_back({ VEC3(width/2,  width/2, -width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(1.f, 0.f, 0.f), VEC2(1.f, 1.f) });
-	vertices.push_back({ VEC3(width/2, -width/2, -width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(1.f, 0.f, 0.f), VEC2(0.f, 1.f) });
-	vertices.push_back({ VEC3(width/2,  width/2,  width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(1.f, 0.f, 0.f), VEC2(1.f, 0.f) });
+	//vertices.push_back({ VEC3(width/2, -width/2, -width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(1.f, 0.f, 0.f), VEC2(0.f, 1.f) });
+	//vertices.push_back({ VEC3(width/2,  width/2,  width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(1.f, 0.f, 0.f), VEC2(1.f, 0.f) });
 	vertices.push_back({ VEC3(width/2, -width/2,  width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(1.f, 0.f, 0.f), VEC2(0.f, 0.f) });
+
+	// 12, 13, 14; 13, 12, 15;
 
 	// Bottom face.
 	vertices.push_back({ VEC3(-width/2, -width/2, -width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, -1.f, 0.f), VEC2(0.f, 1.f) });
 	vertices.push_back({ VEC3( width/2, -width/2, -width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, -1.f, 0.f), VEC2(1.f, 1.f) });
 	vertices.push_back({ VEC3( width/2, -width/2,  width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, -1.f, 0.f), VEC2(1.f, 0.f) });
-	vertices.push_back({ VEC3( width/2, -width/2,  width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, -1.f, 0.f), VEC2(1.f, 0.f) });
+	//vertices.push_back({ VEC3( width/2, -width/2,  width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, -1.f, 0.f), VEC2(1.f, 0.f) });
 	vertices.push_back({ VEC3(-width/2, -width/2,  width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, -1.f, 0.f), VEC2(0.f, 0.f) });
-	vertices.push_back({ VEC3(-width/2, -width/2, -width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, -1.f, 0.f), VEC2(0.f, 1.f) });
+	//vertices.push_back({ VEC3(-width/2, -width/2, -width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, -1.f, 0.f), VEC2(0.f, 1.f) });
+
+	// 16, 17, 18; 18, 19, 16;
 
 	// Up face.
 	vertices.push_back({ VEC3(-width/2,  width/2, -width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, 1.f, 0.f), VEC2(0.f, 1.f) });
 	vertices.push_back({ VEC3( width/2,  width/2,  width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, 1.f, 0.f), VEC2(1.f, 0.f) });
 	vertices.push_back({ VEC3( width/2,  width/2, -width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, 1.f, 0.f), VEC2(1.f, 1.f) });
-	vertices.push_back({ VEC3( width/2,  width/2,  width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, 1.f, 0.f), VEC2(1.f, 0.f) });
-	vertices.push_back({ VEC3(-width/2,  width/2, -width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, 1.f, 0.f), VEC2(0.f, 1.f) });
+	//vertices.push_back({ VEC3( width/2,  width/2,  width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, 1.f, 0.f), VEC2(1.f, 0.f) });
+	//vertices.push_back({ VEC3(-width/2,  width/2, -width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, 1.f, 0.f), VEC2(0.f, 1.f) });
 	vertices.push_back({ VEC3(-width/2,  width/2,  width/2), COLOR4(1.f, 1.f, 1.f, 1.f), VEC3(0.f, 1.f, 0.f), VEC2(0.f, 0.f) });
 
-	for (size_t i = 0; i < 36; i++)
+	// 20, 21, 22; 21, 20, 23;
+
+	indices.insert(indices.end(), { 0,1,2,1,0,3,4,5,6,6,7,4,8,9,10,10,11,8,12,13,14,13,12,15,16,17,18,18,19,16,20,21,22,21,20,23 });
+	/*for (size_t i = 0; i < 36; i++)
 	{
 		indices.push_back(i);
-	}
+	}*/
 }
